99b868834e6212bc0f0299534c3b69bd3afde09f
Vlad <45117884+VOBradley@users.noreply.github.com>
2019-12-11 21:25:24 +0600

    new2

--- 123/js/script.js
+++ 123/js/script.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    $(window).scroll(function(event) {
-        var s=$ (this).scrollTop();
-        var w=$ (this).outerWidth();
-        var h=$ ('.content').outerHeight();
-        var h_b=$ ('.parallax').outerHeight();
-        var p = s/h*100;
-        var p_b = s/h_b*100;
-        var o = 1-1/100*p_b;
+$(document).ready(function () {
+	$(window).scroll(function (event) {
+		var s = $(this).scrollTop();
+		var w = $(this).outerWidth();
+		var h = $('.content').outerHeight();
+		var h_b = $('.parallax').outerHeight();
+		var p = s / h * 100;
+		var p_b = s / h_b * 100;
+		var o = 1 - 1 / 100 * p_b;
 
-        var z_1=1+(w/10000*p_b);
-        $('.parallax__fog').css('transform','scale('+z_1+')');
-        $('.parallax__fog').css('opacity',o);
-    });
+		var z_1 = 1 + (w / 10000 * p_b);
+		$('.parallax__fog').css('transform', 'scale(' + z_1 + ')');
+		$('.parallax__fog').css('opacity', o);
+	});
 });
